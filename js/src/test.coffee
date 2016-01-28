113db52012f85bcf62f0f169dfdc4574c6933a39
Jigmet Wangchuk <orangeorcs@gmail.com>
2016-01-28 17:07:17 +0530

    highligh cell and its neighbours

--- js/src/test.coffee
+++ js/src/test.coffee
@@ -3,7 +3,7 @@ Game =
   randomShapeClass: ->
     "fa-" + Game.shapes[Math.floor(Math.random()*Game.shapes.length)]
   populateCellsWithShapes: ->
-    $.each $(".cell i"), (i, ele) -> $(ele).addClass Game.randomShapeClass
+    $.each $(".cell i"), (i, ele) -> $(ele).addClass(Game.randomShapeClass).addClass('animated').addClass('infinite')
   popualateCellCoordinates: ->
     rowNo = 1
     colNo = 1
@@ -18,13 +18,40 @@ Game =
       rowNo++
     Game.rowsCount = rowNo
     Game.columnsCount = colNo
+  fetchCell: (rowNo, colNo) ->
+    selector = ".cell"
+    selector += "[data-row-no='#{rowNo}']"
+    selector += "[data-col-no='#{colNo}']"
+    $(selector)
+  highlightCell: (cell) ->
+    $(cell).children('i').addClass('jello')
+  selectCell: (cell) ->
+    if Game.selectedCell == null
+      Game.selectedCell = cell
+      $(cell).children('i').addClass('flash')
+      colNo = parseInt(cell.dataset.colNo)
+      rowNo = parseInt(cell.dataset.rowNo)
+      Game.highlightCell(Game.fetchCell(rowNo-1, colNo))
+      Game.highlightCell(Game.fetchCell(rowNo+1, colNo))
+      Game.highlightCell(Game.fetchCell(rowNo, colNo-1))
+      Game.highlightCell(Game.fetchCell(rowNo, colNo+1))
+    else
+      Game.deselectCell()
+  deselectCell: ->
+    $('.cell i').removeClass('jello').removeClass('flash')
+    Game.selectedCell = null
+  bindCellsForClick: ->
+    $('.cell').click ->
+      Game.selectCell(@)
   checkMatches: ->
     console.log "Checking matches"
   init: ->
     Game.rowsCount = 0
     Game.columnsCount = 0
+    Game.deselectCell()
     Game.populateCellsWithShapes()
     Game.popualateCellCoordinates()
+    Game.bindCellsForClick()
     Game.checkMatches()
 
 $ ->
--- js/test.js
+++ js/test.js
@@ -7,7 +7,7 @@ Game = {
   },
   populateCellsWithShapes: function() {
     return $.each($(".cell i"), function(i, ele) {
-      return $(ele).addClass(Game.randomShapeClass);
+      return $(ele).addClass(Game.randomShapeClass).addClass('animated').addClass('infinite');
     });
   },
   popualateCellCoordinates: function() {
@@ -26,14 +26,50 @@ Game = {
     Game.rowsCount = rowNo;
     return Game.columnsCount = colNo;
   },
+  fetchCell: function(rowNo, colNo) {
+    var selector;
+    selector = ".cell";
+    selector += "[data-row-no='" + rowNo + "']";
+    selector += "[data-col-no='" + colNo + "']";
+    return $(selector);
+  },
+  highlightCell: function(cell) {
+    return $(cell).children('i').addClass('jello');
+  },
+  selectCell: function(cell) {
+    var colNo, rowNo;
+    if (Game.selectedCell === null) {
+      Game.selectedCell = cell;
+      $(cell).children('i').addClass('flash');
+      colNo = parseInt(cell.dataset.colNo);
+      rowNo = parseInt(cell.dataset.rowNo);
+      Game.highlightCell(Game.fetchCell(rowNo - 1, colNo));
+      Game.highlightCell(Game.fetchCell(rowNo + 1, colNo));
+      Game.highlightCell(Game.fetchCell(rowNo, colNo - 1));
+      return Game.highlightCell(Game.fetchCell(rowNo, colNo + 1));
+    } else {
+      return Game.deselectCell();
+    }
+  },
+  deselectCell: function() {
+    $('.cell i').removeClass('jello').removeClass('flash');
+    return Game.selectedCell = null;
+  },
+  bindCellsForClick: function() {
+    return $('.cell').click(function() {
+      return Game.selectCell(this);
+    });
+  },
   checkMatches: function() {
     return console.log("Checking matches");
   },
   init: function() {
     Game.rowsCount = 0;
     Game.columnsCount = 0;
+    Game.deselectCell();
     Game.populateCellsWithShapes();
     Game.popualateCellCoordinates();
+    Game.bindCellsForClick();
     return Game.checkMatches();
   }
 };
