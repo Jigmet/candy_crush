28eee7ce1bc01d9bd65a59bf7b3d0c04d190f5fb
Jigmet Wangchuk <orangeorcs@gmail.com>
2016-01-28 15:46:41 +0530

    populate cells with coordinates

--- js/src/test.coffee
+++ js/src/test.coffee
@@ -1,9 +1,31 @@
 Game =
-  shapes: ["heart", "star", "square", "circle", "car", "rocket"]
+  shapes: ["heart", "star", "square", "circle", "rocket", "car"]
   randomShapeClass: ->
     "fa-" + Game.shapes[Math.floor(Math.random()*Game.shapes.length)]
-  init: ->
+  populateCellsWithShapes: ->
     $.each $(".cell i"), (i, ele) -> $(ele).addClass Game.randomShapeClass
+  popualateCellCoordinates: ->
+    rowNo = 1
+    colNo = 1
+    $.each $("#board .row")
+    , (i, row) ->
+      colNo = 1
+      $.each $(row).children('.cell')
+      , (j, cell) ->
+        cell.dataset.rowNo = rowNo
+        cell.dataset.colNo = colNo
+        colNo++
+      rowNo++
+    Game.rowsCount = rowNo
+    Game.columnsCount = colNo
+  checkMatches: ->
+    console.log "Checking matches"
+  init: ->
+    Game.rowsCount = 0
+    Game.columnsCount = 0
+    Game.populateCellsWithShapes()
+    Game.popualateCellCoordinates()
+    Game.checkMatches()
 
 $ ->
   Game.init()
--- js/test.js
+++ js/test.js
@@ -1,14 +1,40 @@
 var Game;
 
 Game = {
-  shapes: ["heart", "star", "square", "circle", "car", "rocket"],
+  shapes: ["heart", "star", "square", "circle", "rocket", "car"],
   randomShapeClass: function() {
     return "fa-" + Game.shapes[Math.floor(Math.random() * Game.shapes.length)];
   },
-  init: function() {
+  populateCellsWithShapes: function() {
     return $.each($(".cell i"), function(i, ele) {
       return $(ele).addClass(Game.randomShapeClass);
     });
+  },
+  popualateCellCoordinates: function() {
+    var colNo, rowNo;
+    rowNo = 1;
+    colNo = 1;
+    $.each($("#board .row"), function(i, row) {
+      colNo = 1;
+      $.each($(row).children('.cell'), function(j, cell) {
+        cell.dataset.rowNo = rowNo;
+        cell.dataset.colNo = colNo;
+        return colNo++;
+      });
+      return rowNo++;
+    });
+    Game.rowsCount = rowNo;
+    return Game.columnsCount = colNo;
+  },
+  checkMatches: function() {
+    return console.log("Checking matches");
+  },
+  init: function() {
+    Game.rowsCount = 0;
+    Game.columnsCount = 0;
+    Game.populateCellsWithShapes();
+    Game.popualateCellCoordinates();
+    return Game.checkMatches();
   }
 };
 
